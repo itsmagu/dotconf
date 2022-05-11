fcfb975f2f9300d0c6d686a207c6e9b231db6860
magu <95685391+itsmagu@users.noreply.github.com>
2022-05-11 20:48:57 +0200

    pnpm

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,3 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
+
+setxkbmap se -option 'caps:escape'
+
+set PNPM_HOME "home/"whoami"/.local/share/pnpm"
+set PATH "$PATH:$PNPM_HOME"
